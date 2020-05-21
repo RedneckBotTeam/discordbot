52ef9d636b457f3a6d50a22ef270cfa926af2632
itsszeco <65229567+itsszeco@users.noreply.github.com>
2020-05-22 01:35:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -85,12 +85,16 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                             url: "https://i.pinimg.com/originals/b4/01/96/b40196385128d64ba3a111fc61faa054.jpg"
                         }
                     }
-                });
+                }),
              bot.addToRole({
                  "guild_id": 380734703034040331,
                  "roles": 709533684197228644,
                  "user": user,
-
+		}),
+		      bot.sendMessage({
+                    to: channelID,
+                    message: "<@&707333518581039105> Shame on you",
+		});
 	     })
             break;
          }

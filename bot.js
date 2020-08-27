a117bb59cc66b156fb9783c5c6521c21f3f30ec2
Bartosz Wachowski <barteki12@hotmail.com>
2020-08-27 21:33:12 +0200

    Auth token env variable

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ var Discord = require('discord.io');
 var logger = require('winston');
 // Bot Reads auth.json file to get token
 const dotenv = require('dotenv');
-var auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -12,7 +11,7 @@ logger.add(new logger.transports.Console, {
 logger.level = 'all';
 // Initialize Discord Bot
 var bot = new Discord.Client({
-    token: auth.token,
+    token: ENV.token,
     autorun: true
 });
 bot.on('ready', function (evt) {
